9e228f47489475f3fd52ee1cf14cd1e2d8e2e980
giulia97-w <72927421+giulia97-w@users.noreply.github.com>
2023-06-05 17:56:17 +0200

    Add files via upload

---
+++ src/main/Ticket.java
@@ -0,0 +1,279 @@
+package main;
+
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import weka.weka;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.System.Logger.Level;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class Ticket {
+	private static final Logger logger =  Logger.getLogger(Ticket.class.getName());
+
+    private String id;
+    private Integer fixedVersion;
+    private Integer openVersion; 
+    private Integer injectedVersion; //
+    private List<Integer> affectedVersion; 
+    private Integer index;
+    
+    private LocalDateTime resolutionDate;
+
+    private LocalDateTime creationDate;
+    private List<LocalDateTime> commitDateList;
+    private List<RevCommit> commitList;
+    private List<String> fileList;
+
+	private int p;
+
+
+    public Ticket (String id, LocalDateTime creationDate, List<Integer> av)
+    {
+        this.id = id;
+        this.affectedVersion = av;
+        this.commitList = new ArrayList<>();
+        this.fileList = new ArrayList<>();
+        this.creationDate = creationDate;
+    }
+
+  
+    public String getTicketID() {
+        return id;
+    }
+    
+    public int getP(){
+        return this.p;
+    }
+    public void setP(int p)
+    {
+        this.p=p;
+    }
+    
+
+    public List<Integer> getAffectedVersion() {
+        return affectedVersion;
+    }
+    public void setAffectedVersion(List<Integer> av) {
+        this.affectedVersion = av;
+    }
+
+    public LocalDateTime getResolutionDate() {
+        return resolutionDate;
+    }
+    public void setResolutionDate(LocalDateTime resolutionDate) {
+        this.resolutionDate = resolutionDate;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+    
+    
+
+
+    public Integer getFixedVersion() {
+        return fixedVersion;
+    }
+    public void setFixedVersion(Integer fv) {
+        this.fixedVersion = fv;
+    }
+
+    public Integer getOpenVersion() {
+        return openVersion;
+    }
+    public void setOpenVersion(Integer ov) {
+        this.openVersion = ov;
+    }
+
+    public Integer getInjectedVersion() {
+        return injectedVersion;
+    }
+    public void setInjectedVersion(Integer iv) {
+        this.injectedVersion = iv;
+    }
+    
+    public List<LocalDateTime> getCommitDateList() {
+        return commitDateList;
+    }
+    public void setCommitDateList(List<LocalDateTime> commitDateList) {
+        this.commitDateList = commitDateList;
+    }
+
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public List<String>  getFileList() { 
+    	return fileList;
+    	}
+    
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public List<RevCommit> getCommitList() {
+        return commitList;
+    }
+
+    public static List<Ticket> getTickets(List<Release> releases, String projName) throws IOException {
+		List<Ticket> tickets = new ArrayList<>();
+        Integer i = 0;
+        Integer total = 1;
+        while (i < total) {
+            Integer j = i + 1000;
+            String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22" + projName +
+                    "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR%22status%22=%22resolved%22)" +
+                    "AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,affectedVersion,versions,created&startAt="
+                    + i + "&maxResults=" + j.toString();
+            JSONObject json = Release.readJsonFromUrl(url);
+            JSONArray issues = json.getJSONArray("issues");
+            total = json.getInt("total");
+            for (; i < total && i < j; i++) {
+                JSONObject jsonIssues = issues.getJSONObject(i % 1000);
+                Ticket ticket = createTicket(jsonIssues, releases);
+                tickets.add(ticket);
+            }
+        }
+        return tickets;
+    }
+
+    private static Ticket createTicket(JSONObject jsonIssues, List<Release> releases) {
+        JSONObject jsonFields = jsonIssues.getJSONObject("fields");
+        String key = jsonIssues.get("key").toString();
+        LocalDateTime creationDate = LocalDateTime.parse(jsonFields.getString("created").substring(0, 16));
+        JSONArray affectedVersions = jsonFields.getJSONArray("versions");
+        List<Integer> affectedVersionsIndexList = Release.getMatchingReleaseIndexes(affectedVersions, releases);
+        Ticket ticket = new Ticket(key, creationDate, affectedVersionsIndexList);
+        return updateTicket(ticket, affectedVersionsIndexList, creationDate, releases);
+    }
+    
+    public static Ticket updateTicket(Ticket ticket, List<Integer> affectedVersionsIndexList, LocalDateTime creationDate, List<Release> releases) {
+        int injectedVersion = affectedVersionsIndexList.isEmpty() || affectedVersionsIndexList.get(0) == null ? 0 : affectedVersionsIndexList.get(0);
+        ticket.setInjectedVersion(injectedVersion);
+        ticket.setOpenVersion(RetrieveMetrics.afterBeforeDate(creationDate, releases));
+        return ticket;
+    }
+    public static final String PROJECT = "BOOKKEEPER";
+	public static final String PROJECT1 = "OPENJPA";
+    
+	public static void ticketDatasetBookkeeper(List<Ticket> ticketList) {
+	  logger.info("Creando file BOOKKEEPERTickets.csv");
+
+  	  try (
+  	   FileWriter fileWriter = new FileWriter(PROJECT + "Tickets"+".csv")) {
+  	   
+  	   fileWriter.append("TICKET ID ; IV ; OV ; FV ; AV \n");
+  	   for (Ticket ticket : ticketList) {
+  		   fileWriter.append(ticket.getTicketID());
+  		   fileWriter.append(";");
+  		   fileWriter.append(ticket.getInjectedVersion().toString());
+  		   fileWriter.append(";");
+  		   fileWriter.append(ticket.getOpenVersion().toString());
+  		   fileWriter.append(";");
+  		   fileWriter.append(ticket.getFixedVersion().toString());
+  		   fileWriter.append(";");
+  		   fileWriter.append(ticket.getAffectedVersion().toString());
+  		   fileWriter.append("\n");
+  	   }
+  	   
+  	  } catch (Exception ex) {
+  		  logger.info("Errore nella creazione del dataset");
+  	  
+  	  	}
+  	 }	
+	public static void ticketDatasetAvro(List<Ticket> ticketList) {
+		  logger.info("Creando file AvroTickets.csv");
+
+	  	  try (
+	  	   FileWriter fileWriter = new FileWriter("AVRO" + "Tickets"+".csv")) {
+	  	   
+	  	   fileWriter.append("TICKET ID ; IV ; OV ; FV ; AV \n");
+	  	   for (Ticket ticket : ticketList) {
+	  		   fileWriter.append(ticket.getTicketID());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getInjectedVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getOpenVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getFixedVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getAffectedVersion().toString());
+	  		   fileWriter.append("\n");
+	  	   }
+	  	   
+	  	  } catch (Exception ex) {
+	  		  logger.info("Errore nella creazione del dataset");
+	  	  
+	  	  	}
+	  	 }
+	
+	public static void ticketDatasetAccumulo(List<Ticket> ticketList) {
+		  logger.info("Creando file Zoo	keeperTickets.csv");
+
+	  	  try (
+	  	   FileWriter fileWriter = new FileWriter("ZOOKEEPER" + "Tickets"+".csv")) {
+	  	   
+	  	   fileWriter.append("TICKET ID ; IV ; OV ; FV ; AV \n");
+	  	   for (Ticket ticket : ticketList) {
+	  		   fileWriter.append(ticket.getTicketID());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getInjectedVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getOpenVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getFixedVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getAffectedVersion().toString());
+	  		   fileWriter.append("\n");
+	  	   }
+	  	   
+	  	  } catch (Exception ex) {
+	  		  logger.info("Errore nella creazione del dataset");
+	  	  
+	  	  	}
+	  	 }	
+	
+	public static void ticketDatasetOpenjpa(List<Ticket> ticketList) {
+	    logger.info("Creando file OPENJPATickets.csv");
+
+	  	  try (
+	  	   FileWriter fileWriter = new FileWriter(PROJECT1 + "Tickets"+".csv")) {
+	  	   
+	  	   fileWriter.append("TICKET ID ; IV ; OV ; FV ; AV \n");
+	  	   for (Ticket ticket : ticketList) {
+	  		   fileWriter.append(ticket.getTicketID());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getInjectedVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getOpenVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getFixedVersion().toString());
+	  		   fileWriter.append(";");
+	  		   fileWriter.append(ticket.getAffectedVersion().toString());
+	  		   fileWriter.append("\n");
+	  	   }
+	  	   
+	  	  } catch (Exception ex) {
+	  		  logger.info("Errore nella creazione del dataset");
+
+	  	  
+	  	  	}
+	  	 }
+
+
+
+	
+
+}
---
+++ src/weka/ClassifierInfo.java
@@ -0,0 +1,180 @@
+package weka;
+
+
+
+public class ClassifierInfo 
+{
+	private final String datasetInfo;
+	private String classifier;
+	private Double numberTrain;
+	private Double buggyInTrain;
+	private Double buggyInTest;
+
+	private String balancing;
+	private String featureSelection;
+	private String sensitivity;
+	private Integer fp;
+	private Integer tp;
+	private Integer fn;
+	private Integer tn;
+	private Integer release;
+	private Double precision;
+	private Double recall;
+	private Double auc;
+	private Double kappa;
+	
+
+
+
+	//get
+	public ClassifierInfo(String dataset)
+	{
+		this.datasetInfo = dataset;
+	}
+	
+	public String getClassifierType()
+	{
+		return this.classifier;
+	}
+	public String getBalancing()
+	{
+		return this.balancing;
+	}
+	public String getFeatureSelection()
+	{
+		return this.featureSelection;
+	}
+	public String getSensitivity()
+	{
+		return this.sensitivity;
+	}
+	public Integer getFp()
+	{
+		return this.fp;
+	}
+	public Integer getTn()
+	{
+		return this.tn;
+	}
+	public Integer getFn()
+	{
+		return this.fn;
+	}
+	public Double getRecall()
+	{
+		return this.recall;
+	}
+	public Double getPrecision()
+	{
+		return this.precision;
+	}
+	public Double getNumberTrain()
+	{
+		return this.numberTrain;
+	}
+	public Double getBuggyInTrain()
+	{
+		return this.buggyInTrain;
+	}
+	public Double getKappa()
+	{
+		return this.kappa;
+	}
+	public Double getBuggyInTest()
+	{
+		return this.buggyInTest;
+	}
+	public Double getAuc()
+	{
+		return this.auc;
+	}
+
+	//set
+
+
+	public void setClassifierType(String classifier)
+	{
+		this.classifier = classifier;
+	}
+
+	public void setBalancingMethod(String balancing)
+	{
+		this.balancing = balancing;
+	}
+
+	public void setFeatureSelectionMethod(String f)
+	{
+		this.featureSelection = f;
+	}
+
+	public void setSensitivityMethod(String s)
+	{
+		this.sensitivity = s;
+	}
+	public Integer getRelease()
+	{
+		return this.release;
+	}
+	public void setReleaseNumber(Integer release)
+	{
+		this.release = release;
+	}
+	public Integer getTp()
+	{
+		return this.tp;
+	}
+	public void setTruePositives(Integer t)
+	{
+		this.tp = t;
+	}
+
+	public void setFalsePositives(Integer f)
+	{
+		this.fp = f;
+	}
+
+	public void setTrueNegatives(Integer t)
+	{
+		this.tn = t;
+	}
+
+	public void setFalseNegatives(Integer f)
+	{
+		this.fn = f;
+	}
+
+	public void setRecall(Double recall)
+	{
+		this.recall = recall;
+	}
+
+	public void setPrecision(Double precision)
+	{
+		this.precision = precision;
+	}
+
+	public void setKappa(Double kappa)
+	{
+		this.kappa = kappa;
+	}
+
+	public void setAuc(Double auc)
+	{
+		this.auc = auc;
+	}
+
+	public void setNumberTrainTest(Double numberTrain)
+	{
+		this.numberTrain = numberTrain;
+	}
+
+	public void setBuggyInTest(Double buggyInTest)
+	{
+		this.buggyInTest = buggyInTest;
+	}
+
+	public void setBuggyInTrain(Double buggyInTrain)
+	{
+		this.buggyInTrain = buggyInTrain;
+	}
+}
